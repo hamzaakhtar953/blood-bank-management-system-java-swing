bc352bb7a081af665442a06e2b74cd0d420adf12
Ammar-Aslam <ammarfast123@gmail.com>
2019-07-13 14:42:33 +0500

    Created Project

---
+++ Blood Bank Management System Java Swing/src/SourceCode/Test.java
@@ -0,0 +1,23 @@
+package SourceCode;
+
+public class Test {
+    String name;
+    int age;
+    
+    public Test()
+    {
+        name="Ammar";
+        age=23;
+    }
+    
+    public String GetName()
+    {
+        return name;
+    }
+    
+    public int GetAge()
+    {
+        return age;
+    }
+    
+}
