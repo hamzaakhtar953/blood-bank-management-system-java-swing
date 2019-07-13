ea60c8f314105253adcf1d6abd0bf920b95b8b0e
hamzaakhtar <hamzaakhtar0@gmail.com>
2019-07-13 12:08:14 -0700

    Added User class

---
+++ Blood Bank Management System Java Swing/src/SourceCode/User.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SourceCode;
+
+/**
+ *
+ * @author Ali
+ */
+public class User {
+
+    private String name;
+    private String address;
+    private String emailId;
+    private Integer phoneNumber;
+
+    public User(String name, String address, String emailId, Integer phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.emailId = emailId;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return (name + " lives at " + address + "\n"
+                + "Contact details: \n\n"
+                + "Email: " + emailId + "\n"
+                + "Contact: " + phoneNumber);
+    }
+}
